d865c27e30e36acb88e9299ce16f2ca188506a78
nsw11 <39717120+nsw11@users.noreply.github.com>
2022-12-09 14:19:41 -0800

    added functionality to compared guesses to all solutions for preprocessing

--- wordle.py
+++ wordle.py
@@ -1,15 +1,18 @@
 from wordle_functions import *
 
 def infogain (guess,word): #filler function please replace
+    # should have a higher number based on information gained
     return 1
 
-guesses = read_csv('valid_guesses.csv',True)
-solutions = read_csv('valid_solutions.csv',True)
+guesses = read_csv('valid_guesses.csv')
+solutions = read_csv('valid_solutions.csv')
 
-
-for guess in guesses:
-    total = 0
-    for word in solutions:
-        total += infogain(guess,word)
+gains = [[0 for x in range(len(solutions))] for x in range(len(guesses))] # secondary index(solutions) on inside, first index on outside(guesses)
+for g_index, guess in enumerate(guesses):
+    print("line number "+ str(g_index)+ ": "+str(guess))
+    
+    for w_index, word in enumerate(solutions):
+        gains[g_index][w_index] = infogain(guess,word)
         # need to implement pushing values to vector instead of accumulating
-    avg = total/ len(solutions)
+    print(sum(gains[g_index])) # should sum all values of a given word in the guess set, before infogain() is completed this should just return the amount of elements in the solution set
+    
--- wordle_functions.py
+++ wordle_functions.py
@@ -1,6 +1,6 @@
 import csv 
 
-def read_csv(path,printresults= False):
+def read_csv(path,printresults= False): # reads a given csv file and formats it into an array, returns array
     output = []
     with open(path) as csvfile:    
         csvReader = csv.reader(csvfile)   
@@ -11,3 +11,11 @@ def read_csv(path,printresults= False):
     print("read file of size "+ str(len(output)))
     print('FINISHED READING FILE '+ str(path))
     return output
+
+
+def calc_avg(arr): #takes an array and calculates the average
+    return sum(arr)/len(arr)
+
+def calc_worst(arr):# the worst value should be the one with the least info gained
+    return min(arr)
+    
