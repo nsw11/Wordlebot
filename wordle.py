080641e78202cb9e162abaa295fac3a07412c63a
nsw11 <39717120+nsw11@users.noreply.github.com>
2022-12-09 14:58:23 -0800

    added functionality to write to CSV file (WIP)

--- wordle.py
+++ wordle.py
@@ -9,10 +9,11 @@ solutions = read_csv('valid_solutions.csv')
 
 gains = [[0 for x in range(len(solutions))] for x in range(len(guesses))] # secondary index(solutions) on inside, first index on outside(guesses)
 for g_index, guess in enumerate(guesses):
-    print("line number "+ str(g_index)+ ": "+str(guess))
+    #print("line number "+ str(g_index)+ ": "+str(guess))
     
     for w_index, word in enumerate(solutions):
-        gains[g_index][w_index] = infogain(guess,word)
+        gains[g_index][w_index] = g_index
         # need to implement pushing values to vector instead of accumulating
-    print(sum(gains[g_index])) # should sum all values of a given word in the guess set, before infogain() is completed this should just return the amount of elements in the solution set
-    
+    #print(sum(gains[g_index])) # should sum all values of a given word in the guess set, before infogain() is completed this should just return the amount of elements in the solution set
+
+write_csv("results.csv",gains)
--- wordle_functions.py
+++ wordle_functions.py
@@ -1,17 +1,25 @@
 import csv 
 
 def read_csv(path,printresults= False): # reads a given csv file and formats it into an array, returns array
+# guidence for this function provided by https://www.folkstalk.com/2022/10/python-read-csv-into-array-with-code-examples.html
     output = []
     with open(path) as csvfile:    
-        csvReader = csv.reader(csvfile)   
+        csvReader = csv.reader(csvfile,delimiter=',')   
         for row in csvReader:
             output.append(row[0])
     if (printresults):
         print(output)
     print("read file of size "+ str(len(output)))
-    print('FINISHED READING FILE '+ str(path))
+    print(' FINISHED READING FILE '+ str(path))
     return output
 
+def write_csv(path,list):
+    with open(path,"w",newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for i in range(len(list)):
+            csvwriter.writerow(list[i])
+    print("Finished writing file "+path +" with "+  str(len(list)) + " elements")
+            
 
 def calc_avg(arr): #takes an array and calculates the average
     return sum(arr)/len(arr)
